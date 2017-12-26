e8855d36b9975d9bce8a92366e33005f7bc93bc9
loveincode <huonefl@163.com>
2017-12-26 23:20:25 +0800

    nodejs

---
+++ 14 - 前端技术/NodeJS/server.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+http.createServer(function (request, response) {
+
+    // 发送 HTTP 头部 
+    // HTTP 状态值: 200 : OK
+    // 内容类型: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    // 发送响应数据 "Hello World"
+    response.end('Hello World\n');
+}).listen(8888);
+
+// 终端打印如下信息
+console.log('Server running at http://127.0.0.1:8888/');
